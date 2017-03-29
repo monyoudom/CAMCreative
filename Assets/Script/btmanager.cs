9cee63bdf53051663ea2e8860b5fc9d591312c33
Machintos-HD <machintos-hd@Machintos-HDs-MacBook-Pro.local>
2017-03-29 20:26:44 +0700

    first

---
+++ Assets/Script/btmanager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class btmanager : MonoBehaviour {
+	public void newGamebt(string newGamel1){
+		SceneManager.LoadScene (newGamel1);
+	}
+		
+
+}
